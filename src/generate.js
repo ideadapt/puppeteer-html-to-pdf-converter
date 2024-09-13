5b79986ab3fde6a532ca9a6697c54d0bead1bd51
duduyo <duduyoyo63-dev@yahoo.fr>
2024-09-13 10:32:10 +0200

    feat: Return an error with appropriate status code (#3)
    Co-authored-by: Lionel DURAND <lioneldurand@gmail.com>

--- src/generate.js
+++ src/generate.js
@@ -1,15 +1,14 @@
-const { validationResult } = require('express-validator');
+const {validationResult} = require('express-validator');
 
-module.exports = async function(req, res) {
-    if(!req.headers['content-type']){
+module.exports = async function (req, res) {
+    if (!req.headers['content-type']) {
         return res.status(406).json({success: false, errors: [{msg: 'Missing content-type header'}]})
     }
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({
-            success: false,
-            errors: errors.errors
+            success: false, errors: errors.errors
         });
     }
 
@@ -18,16 +17,11 @@ module.exports = async function(req, res) {
     }
 
     const pdfOptions = {
-        format: 'A4',
-        margin: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
+        format: 'A4', margin: {
+            top: 0, right: 0, bottom: 0, left: 0
         }
     };
-    const availableOptions = ['scale', 'displayHeaderFooter', 'headerTemplate', 'footerTemplate', 'printBackground', 'landscape', 
-    'pageRanges', 'format', 'width', 'height', 'margin.top', 'right', 'margin.bottom', 'margin.left', 'preferCSSPageSize'];
+    const availableOptions = ['scale', 'displayHeaderFooter', 'headerTemplate', 'footerTemplate', 'printBackground', 'landscape', 'pageRanges', 'format', 'width', 'height', 'margin.top', 'right', 'margin.bottom', 'margin.left', 'preferCSSPageSize'];
     const integerOptions = ['scale', 'width', 'height'];
     for (const option of availableOptions) {
         if (req.body[option] && !option.includes('margin')) {
@@ -42,45 +36,59 @@ module.exports = async function(req, res) {
         }
     }
 
-    const page = await global.browser.newPage();
-    if (req.body['viewportDimensions']) {
-        await page.setViewport({
-            width: Number(req.body['viewportDimensions'].split('x')[0]),
-            height: Number(req.body['viewportDimensions'].split('x')[1])
-          });
+    try {
+        const page = await global.browser.newPage();
+
+        if (req.body['viewportDimensions']) {
+            await page.setViewport({
+                width: Number(req.body['viewportDimensions'].split('x')[0]),
+                height: Number(req.body['viewportDimensions'].split('x')[1])
+            });
         }
 
-    if (req.body.url) {
-        await page.goto(req.body.url, { waitUntil: req.body['waitUntil'] ?? 'networkidle0' });
+        if (req.body.url) {
+            let response = await page.goto(req.body.url, {waitUntil: req.body['waitUntil'] ?? 'networkidle0'});
+            if (response.status() >= 400) {
+                return res.status(400).json({success: false, errors: [{msg: `Cannot access URL ${req.body.url}, the server returned an error ${response.status()}`}]})
+            }
+        } else {
+            await page.setContent(req.body.html);
+        }
+
         if (req.body['scrollPage']) {
-            await autoScroll(page);
-            await page.waitForNetworkIdle();
+            await scrollPage(page);
         }
-    } else {
-        await page.setContent(req.body.html);
+
+        const bytes = await page.pdf(pdfOptions);
+        await page.close();
+
+        res.set("Content-Type", "application/octet-stream")
+        res.set("Content-Disposition", `attachment;filename=${req.body['filename'] || 'generated-file'}.pdf`)
+        return res.status(200).send(Buffer.from(bytes, 'binary'))
+    } catch (error) {
+        await page.close().catch((e) => {
+            console.error(e)
+        });
+        return res.status(500).json({success: false, errors: [{msg: `Internal error : ${error.message}`}]})
     }
-
-    const bytes = await page.pdf(pdfOptions);
-    await page.close();
-
-    res.set("Content-Type", "application/octet-stream")
-    res.set("Content-Disposition", `attachment;filename=${req.body['filename'] || 'generated-file'}.pdf`)
-    return res.status(200).send(Buffer.from(bytes, 'binary'))
 }
 
-async function autoScroll(page) {
+async function scrollPage(page) {
+    // scroll to the end of page step by step
     await page.evaluate(async () => {
-      await new Promise((resolve) => {
-        let totalHeight = 0;
-        const step = 500;
-        const interval = setInterval(() => {
-          window.scrollBy(0, step);
-          totalHeight += step;
-          if (totalHeight >= document.body.scrollHeight) {
-            clearInterval(interval);
-            resolve();
-          }
-        }, 10);
-      });
+        await new Promise((resolve) => {
+            let totalHeight = 0;
+            const step = 500;
+            const intervalId = setInterval(() => {
+                window.scrollBy(0, step);
+                totalHeight += step;
+                if (totalHeight >= document.body.scrollHeight) {
+                    clearInterval(intervalId);
+                    resolve();
+                }
+            }, 10);
+        });
     });
-  }
+    // and wait for networkIdle (lazy load cases)
+    await page.waitForNetworkIdle();
+}
